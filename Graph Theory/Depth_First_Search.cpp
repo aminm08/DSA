eeab78b16e71ec1058ef47f40e37319c479d8963
unknown <maf081378@gmail.com>
2024-05-22 11:29:46 +0330

    Refactor & Update comments

--- Graph Theory/Depth_First_Search.cpp
+++ Graph Theory/Depth_First_Search.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <vector>
-#include <list>
 
 using namespace std;
 
 
-void DFS(vector<list<int>>& AL, vector<bool>& visited, int v){
+void DFS(vector<vector<int>>& adjList, vector<bool>& visited, int v){
 
-    // Depth-First Search (DFS) function to traverse a graph represented by an adjacency list
-    // Parameters:
-    //   - AL: Adjacency list representing the graph
-    //   - visited: Vector to keep track of visited vertices
-    //   - v: Current vertex being visited
+     /**
+     * @brief Performs a depth-first search (DFS) on a graph.
+     * 
+     * @param adjList Adjacency list representation of the graph.
+     * @param visited Vector to keep track of visited nodes.
+     * @param v Starting vertex for the BFS.
+     */
 
     visited[v] = true;
     cout << v << endl;
 
-    for (int neighbour: AL[v]){
+    for (int neighbour: adjList[v]){
         
         if (!visited[neighbour]){
-            DFS(AL, visited, neighbour);
+            DFS(adjList, visited, neighbour);
         }
     }
-
 }
 
 
@@ -30,14 +30,13 @@ int main(){
 
     int V = 3; // number of vertices
 
-    vector<list<int>> AL(V);
+    vector<vector<int>> adjList = {{1, 2}, {0}, {0}};
     vector<bool> visited(V, false);
 
-    AL = {{1, 2}, {0}, {0}};
+    
 
-    DFS(AL, visited, 0);
+    DFS(adjList, visited, 0);
 
     return 0;
-
 
 }
