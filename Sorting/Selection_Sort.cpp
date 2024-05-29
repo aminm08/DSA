856d6999cd285ea14a2e41c0e0fe0b5d228f2996
unknown <maf081378@gmail.com>
2024-05-29 17:59:46 +0330

    Implement Selection sort algorithm

---
+++ Sorting/Selection_Sort.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+
+void swap(int arr[], int index1, int index2) {
+    
+    /*** @brief Swaps two elements in an array.*/
+     
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+
+}
+
+
+void Selection_Sort(int arr[], int n) {
+
+    /**
+     * @brief Sorts an array using the selection sort algorithm.
+     * @param arr The array to sort.
+     * @param n The number of elements in the array.
+     */
+
+    int minIndex ;
+
+    for(int i = 0; i < n - 1; ++i) {
+
+        minIndex  = i;
+
+        for (int j = i + 1; j < n - 1; ++j) {
+
+            if(arr[j] < arr[minIndex ]) {
+                minIndex  = j;
+            }
+        }
+
+        // Don't swap if the min is the first element
+        if (minIndex  != i) {
+            swap(arr, minIndex , i);
+        }
+
+    }
+}
+
+
+void printArray(const int arr[], int n) {
+    
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main(){
+
+    int arr[] = {5, 3, 4, 8, 1, 2, 6};
+    
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, n);
+
+    Selection_Sort(arr, n);
+
+    printArray(arr, n);
+
+    return 0;
+}
