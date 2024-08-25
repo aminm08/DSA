9bab777f3e72958af84d277f0594a8dc871bc242
Amin Forouzan <maf081378@gmail.com>
2024-08-25 12:13:04 +0330

    Refactor merge sort

--- src/Sorting/Merge_Sort.rs
+++ src/Sorting/Merge_Sort.rs
@@ -2,58 +2,41 @@
 
 fn merge(arr: &mut [i32], s: usize, m: usize, e: usize) {
 
+    let left = arr[s..=m].to_vec();
+    let right = arr[m+1..=e].to_vec();
+
+    let (mut l, mut r, mut i) = (0, 0, s);
 
 
-    let mut i = 0usize;
-    let mut j = 0usize;
-    let mut k = 0usize;
+    while l < left.len() && r < right.len() {
 
-    let n1 = m - s + 1;
-    let n2 = e - m;
-    let mut left = vec![0; n1];
-    let mut right = vec![0; n2];
-    
-    left[..].clone_from_slice(&arr[..n1]);
-    println!("{:?}", left);
-    if n2 + n1 == arr.len() {
+        if left[l] < right[r] {
 
-        right[..].clone_from_slice(&arr[n1..]);
-    } else {
-        right[..].clone_from_slice(&arr[n1..(n2+1)]);
-    }
-    println!("{:?}", right);
-    while i < n1 && j < n2 {
-
-        if left[i] < right[j] {
-
-            arr[k] = left[i];    
-            i += 1;
+            arr[i] = left[l];    
+            l += 1;
         } else {
 
-            arr[k] = right[j]; 
-            j += 1;
+            arr[i] = right[r]; 
+            r += 1;
         }
 
-        k += 1;
-
-    }
-
-
-    while i < n1 {
-
-        arr[k] = left[i];
-        k += 1;
         i += 1;
+
     }
 
-    while j < n2 {
+    while l < left.len() {
 
-        arr[k] = right[j];
-        k += 1;
-        j += 1;
+        arr[i] = left[l];
+        i += 1;
+        l += 1;
     }
 
-    
+    while r < right.len() {
+
+        arr[i] = right[r];
+        i += 1;
+        r += 1;
+    }
 
 }
 
@@ -63,37 +46,24 @@ fn merge_sort(arr: &mut [i32], s: usize, e: usize) {
 
     if s == e {
         return 
-
     }
 
-    
     let m: usize = (s + e) / 2;
 
     merge_sort(arr, s, m);
     merge_sort(arr, m+1, e);
     merge(arr, s, m, e);
 
-
-
-
 }
-
-
-
 
 
 fn main() {
 
+    let mut arr = [87, 56, -10, 2, 8, 19, 25, -9, -25, 1, 12];
 
-    let mut arr: [i32; 9] = [5, 8,1, 0, 9, -10, 7, 4, -3];
-
-
-    merge_sort(&mut arr, 0, 9);
+    merge_sort(&mut arr, 0, 10);
 
     println!("{:?}", arr);
-
-
-
 
 }
 
